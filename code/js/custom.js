4f31d616943dfab5d187628b65c2878a6ee7fe26
EnaKrvavac <ena.krvavac@stu.ibu.edu.ba>
2021-05-14 15:59:42 +0200

    update

---
+++ code/js/custom.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+  $("main#spapp > section").height($(document).height() - 60);
+
+  var app = $.spapp({pageNotFound : 'error_404'}); // initialize
+
+
+  app.route({
+    view: 'home',
+    load: 'home.html'
+  });
+
+  app.route({
+    view: 'services',
+    load: 'services.html'
+  });
+
+  app.route({
+    view: 'doctors',
+    load: 'doctors.html'
+  });
+
+  app.route({
+    view: 'make_appointment',
+    load: 'make_appointment.html'
+
+  });
+
+  app.route({
+    view: 'ex2',
+    load: 'ex2.html'
+  });
+
+
+
+
+  // run app
+  app.run();
+
+});
